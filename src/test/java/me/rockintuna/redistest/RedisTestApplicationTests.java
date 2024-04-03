23e99d04fbfc75219025d3f06b48f060260ca0a3
jeong in Lee <rockintuna@e8ight.co.kr>
2024-04-03 13:51:50 +0900

    first commit

---
+++ src/test/java/me/rockintuna/redistest/RedisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.redistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
